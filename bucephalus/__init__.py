3cc6e40cae90a116ebb4dbaa9ae0af58a77b49c7
Alex Thomas <alexthomas00@gmail.com>
2017-10-20 16:45:48 +0100

    Remove unneeded error pages. Improve the nginx config.

--- bucephalus/__init__.py
+++ bucephalus/__init__.py
@@ -20,16 +20,6 @@ data_provider = ViewDataProvider(app.config)
 view_defs = ViewBuilder(data_provider)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
 @app.route('/', methods=['GET'])
 def index():
     return send_from_directory('static', 'index.html')
