b1672f2e10e9c6059836e783d25d1c7eee055df0
Alex Thomas <alexthomas00@gmail.com>
2017-10-23 11:46:43 +0100

    remove token persistence

--- bucephalus/viewdata.py
+++ bucephalus/viewdata.py
@@ -27,25 +27,11 @@ class ViewDataProvider(object):
         logging.info("Getting static data")
         PQTrading.populateStaticData(self._factory)
 
-        logging.info("Getting tokens")
-        # Default to the latest token retrieved
-        token = self.get_tokens()[0]
-
-        logging.info("Setting tokens")
-        self.set_token(token)
-
     def get_tokens(self):
         tokens = self._loader.getRunTokens(datetime.datetime(1990, 1, 1), datetime.datetime.utcnow())
         return [t[0] for t in sorted(tokens, key=lambda x: x[1], reverse=True) if t[0].startswith('[Taurus')][:10]
 
-    def set_token(self, token):
-        logging.debug('Setting token to %s', token)
-        self._token = token
-        t0 = time.time()
-        self._meta_obj = self._loader.getRunMeta(self._token)
-        logging.debug('got meta data in {}s'.format(time.time()-t0))
-
-    def get_view_data(self, query_list, callback):
+    def get_view_data(self, token, query_list, callback):
         """
         Loads multiple queries from the database, calling callback(name, series) for each one
         :param query_list: a set of query objects - a string of series name to load,
@@ -55,7 +41,7 @@ class ViewDataProvider(object):
         queries = [q[0] for q in query_list]
         logging.debug('Calling getRunData with {} queries.'.format(len(queries)))
         t0 = time.time()
-        self._loader.getRunData(self._token, queries, callback)
+        self._loader.getRunData(token, queries, callback)
         logging.debug('getRunData took {}s'.format(time.time()-t0))
 
 
